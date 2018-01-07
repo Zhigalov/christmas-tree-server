c5ce38ee5af20febe8be316db03bc03cd2f4a7d5
Zhigalov <sergey.zhigalov@gmail.com>
2018-01-08 01:01:26 +0500

    :evergreen_tree: express server and mongo connection

---
+++ index.js
@@ -0,0 +1,27 @@
+const app = require('express')();
+const port = 3333;
+const { MongoClient } = require('mongodb');
+
+MongoClient.connect(process.env.MONGO_URI, (err, client) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const measures = client.db('christmas-tree').collection('measures');
+
+  app.get('/register', (req, res) => {
+    const { temp, hum } = req.query;
+
+    measures.insert({
+      temp: parseFloat(temp),
+      hum: parseFloat(hum),
+      ts: new Date()
+    }, () => res.status(204).end());
+  });
+
+  app.listen(port, () => console.log(`Server started on http://localhost:${port}`));
+});
+
+
+'mongodb://temperature-server:cs7gtjpm@ds247327.mlab.com:47327'
